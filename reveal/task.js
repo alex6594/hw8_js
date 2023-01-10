d86159fd0a9ebca4350514bfc74cd5fe59517d36
unknown <alex6594@mail.ru>
2023-01-10 12:50:05 +0300

    rebuilt function

--- reveal/task.js
+++ reveal/task.js
@@ -1,22 +1,14 @@
-const spam = document.getElementsByClassName('reveal');
-
-let isInViewPort = function(element) {
-    const viewPortHeight = window.innerHeight;
-    const elementTop = element.getBoundingClientRect().top;
-
-    if (elementTop < 0 && elementTop > viewPortHeight) {
-        return false;
-    } else {
-        return true;
-    }
-}
-
-window.addEventListener('scroll', function () {
-    for (let i = 0; i < spam.length; i++) {
-        if ( isInViewPort(spam[i]) === true ) {
-            spam[i].classList.add('reveal_active');
-        } else {
-            spam[i].classList.remove('reveal_active');
-        }
-    }
-});
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+        change.target.classList.add('reveal_active');
+      }
+      else change.target.classList.remove('reveal_active');
+    });
+  }
+  let options = { threshold: [1] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.reveal');
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
